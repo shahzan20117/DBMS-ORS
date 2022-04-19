d143a1291f821affba304b86fe1e0f4f9d248c25
Divyansh <divyansh20060@iiitd.ac.in>
2022-04-19 22:04:06 +0530

    test

---
+++ Source/login window.py
@@ -0,0 +1,5 @@
+import mysql.connector
+
+dba = mysql.connector.connect(host = "localhost", user = "customer", passwd = "Cus_pass1@", database = "ORS1")
+
+#ok
--- Source/rough.py
+++
@@ -1,4 +0,0 @@
-x = "abdjsakc781438"
-y = "0012345032498"
-print(x.isdigit())
-print(y.isdigit())
